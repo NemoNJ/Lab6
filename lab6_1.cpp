900840bee0ce312b547179be7d8dca5e2cd4b2a6
NemoNJ <nemojirayu@gmail.com>
2023-12-01 16:51:56 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+int N ,counteven = 0,countodd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    while(N != 0){
+        if(N%2 == 0){
+            counteven += 1;
+        }
+        else{
+            countodd += 1;
+        }
+    cout << "Enter an integer: ";
+    cin >> N;
+    }
+    cout << "#Even numbers = " << counteven <<"\n";
+    cout << "#Odd numbers = "<< countodd ;
     return 0;
 }
