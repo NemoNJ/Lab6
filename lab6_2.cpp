8a5d4530c2559fca85bd90eb2e1888c9c60fc95d
NemoNJ <nemojirayu@gmail.com>
2023-12-01 16:53:33 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double deg2rad(double x){
+    return  M_PI*x/180.0;
+}
+
+double rad2deg(double y){
+    return 180.0*y/M_PI;
+}
+
+double findXComponent(double l1, double l2,double a1,double a2){
+    return l1*cos(a1)+l2*cos(a2);
+}
+
+double findYComponent(double l1, double l2,double a1,double a2){
+    return l1*sin(a1)+l2*sin(a2);
+}
+
+double pythagoras(double xcomp,double ycomp){ //pythagoras function
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+
+void showResult(double result_vec_length, double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = "<< result_vec_length << endl;
+    cout << "Direction of the resultant vector (deg) = "<< result_vec_direction << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+} //void ไม่ต้อง return ตัวอื่นต้อง return
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
